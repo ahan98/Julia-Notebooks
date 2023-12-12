498eec8d0171f5dd76eefb517b9b1e68c9b524be
Alex Han <ahan98@gmail.com>
2023-12-12 14:57:13 -0500

    pdf and html

---
+++ symbolic.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.19.35
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 99c5daba-9927-11ee-38a3-3bc3f474768c
+
+
+# ╔═╡ Cell order:
+# ╠═99c5daba-9927-11ee-38a3-3bc3f474768c
